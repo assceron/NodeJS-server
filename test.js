347f8c8e3da37be4246a3d190888776aa4b699d8
Assunta Cerone <assuntacerone@gmail.com>
2020-04-20 10:08:27 +1000

    Sequential tests

--- test.js
+++ test.js
@@ -1,4 +1,14 @@
-var request = require('request');
+var request = require('request-promise');
+
+function getAggregate(testID){
+    var options = {
+      url: `http://localhost:4567/results/${testID}/aggregate`,
+      method: 'GET',
+  };
+
+  return request(options);
+}
+
 function postXML(xml){
   var headers = {
       'Content-Type': 'text/xml+markr'
@@ -10,67 +20,13 @@ function postXML(xml){
       body: xml
   };
 
-  function callback(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(body);
-      }
-      else{
-        console.error(body);
-      }
-  }
-
-  request(options, callback);
-
-}
-
-function getAggregate(testID){
-    var options = {
-      url: `http://localhost:4567/results/${testID}/aggregate`,
-      method: 'GET',
-  };
-  function callback(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(body);
-      }
-      else{
-        console.error(body)
-      }
-  }
-
-  request(options, callback);
-}
-/*
-  Correct entry
-*/
-function test0(){
-  console.log("Running test #0: Correct format");
-  const xml = `<mcq-test-results>
-                  <mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
-                      <first-name>Jane</first-name>
-                      <last-name>Austen</last-name>
-                      <student-number>521585128</student-number>
-                      <test-id>1234</test-id>
-                      <summary-marks available="20" obtained="13" />
-                  </mcq-test-result>
-              </mcq-test-results>`;
-  postXML(xml);
-
-}
-
-/*
-  Empty XML
-*/
-function test1(){
-  console.log("Running test #1: Empty XML");
-  const xml = "";
-  postXML(xml);
+  return request(options);
 }
 
 /*
   One missing field --> Entire document rejected
 */
-function test2(){
-  console.log("Running test #2: Missing field");
+function test0(){
   const xml = `<mcq-test-results>
                   <mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
                       <first-name>Assunta</first-name>
@@ -90,14 +46,51 @@ function test2(){
 
               </mcq-test-results>`;
 
-  postXML(xml);
+  const mess = "Running test #0: Missing field\n";
+  postXML(xml)
+    .then( response =>{ console.log( mess + response +"\n")})
+    .catch(error => {console.error(mess + error +"\n")})
 }
+
+/*
+  Correct entry
+*/
+function test1(){
+ 
+  const xml = `<mcq-test-results>
+                  <mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
+                      <first-name>Jane</first-name>
+                      <last-name>Austen</last-name>
+                      <student-number>521585128</student-number>
+                      <test-id>1234</test-id>
+                      <summary-marks available="20" obtained="13" />
+                  </mcq-test-result>
+              </mcq-test-results>`;
+
+    const mess = "Running test #1: Correct XML\n" + "Response: ";
+    postXML(xml)
+    .then( response =>{ console.log(mess + response +"\n")})
+    .catch(error => {console.error(mess + error + "\n")})
+  
+}
+
+/*
+  Empty XML
+*/
+function test2(){
+  const xml = "";
+  const mess = "Running test #2: Empty XML\n" + "Response: ";
+  postXML(xml)
+    .then( response =>{ console.log( mess + response +"\n")})
+    .catch(error => {console.error(mess + error +"\n")})
+}
+
+
 
 /*
   Insert same studentID,testID with higher score
 */
 function test3(){
-  console.log("Running test #3: Existent field with higher score");
   const xml = `<mcq-test-results>
                   <mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
                       <first-name>Jane</first-name>
@@ -107,15 +100,21 @@ function test3(){
                       <summary-marks available="20" obtained="14" />
                   </mcq-test-result>
               </mcq-test-results>`;
-  postXML(xml);
+
+  const mess = "Running test #3: Existent field with higher score\n";
+  postXML(xml)
+    .then( response =>{ console.log( mess + response +"\n")})
+    .catch(error => { console.error(mess + error +"\n")})
 }
 
 /*
   Get aggregate from existent test id
 */
 function test4(){
-  console.log("Running test #5: Aggregate results");
-  getAggregate(1234);
+  const mess = "Running test #4: Aggregate results\n";
+  getAggregate(1234)
+    .then( response =>{ console.log( mess + response +"\n")})
+    .catch(error => {console.error(mess + error +"\n")})
 }
 
 /*
@@ -123,13 +122,18 @@ function test4(){
 */
 
 function test5(){
-  console.log("Running test #6: Test ID not available");
-  getAggregate(0);
+  const mess = "Running test #5: Test ID not available\n";
+  getAggregate(0)
+    .then( response =>{ console.log( mess + response +"\n")})
+    .catch(error => {console.error(mess + error +"\n")})
 }
 
-test0();
-test1();
-test2();
-test3();
-test4();
-test5();
+
+async function testing(){
+  const tests = [test0, test1,test2, test3, test4, test5];
+  for (let test of tests){
+    await test()
+  }
+}
+
+testing()
